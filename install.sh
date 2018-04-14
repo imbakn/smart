6338556c4cf82d8e540617ad235b3d48dc6c96da
gaoxingyu <gaoxingyu@huaqin.com>
2018-04-14 16:54:30 +0800

    v0.18

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # mv ~/.smart ~/.smart.bak
 # git clone https://github.com/imbakn/smartcmd ~/.smart
+# git clone https://github.com/imbakn/smart.personal ~/.smart/personal
 
-echo 'export SMART_SHELL=bash' >> ~/.bashrc
-echo '[ -f ~/.smart/main.rc ] && source ~/.smart/main.rc' >> ~/.bashrc
+[ -f ~/.bashrc ] && echo 'export SMART_SHELL=bash' >> ~/.bashrc
+[ -f ~/.bashrc ] && echo '[ -f ~/.smart/main.rc ] && source ~/.smart/main.rc' >> ~/.bashrc
+
+[ -f ~/.zshrc ] && echo 'export SMART_SHELL=zsh' >> ~/.zshrc
+[ -f ~/.zshrc ] && echo '[ -f ~/.smart/main.rc ] && source ~/.smart/main.rc' >> ~/.zshrc
