658239ae9a80fd41ad9a7fdf10e5512907246ff0
gaoxingyu <gaoxingyu@huaqin.com>
2018-04-14 14:57:31 +0800

    v0.13

--- common.rc/cdalias.bash
+++ common.rc/cdalias.bash
@@ -4,11 +4,9 @@ upnum()
 	echo `echo "../" | awk -v num=$1 '{for(i=1;i<=num;i++) printf($0)}'`;
 }
 
-for ((index=0; index<10; ++index)) 
+for ((index=0; index<10; ++index))
 do
     newdir=$(upnum $index);
 	eval D$index=$(upnum $index);
     alias cd$index="cd $newdir";
 done
-
-
--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 # mv ~/.smart ~/.smart.bak
 # git clone https://github.com/imbakn/smartcmd ~/.smart
 
+echo 'export SMART_SHELL=bash' >> ~/.bashrc
 echo '[ -f ~/.smart/main.rc ] && source ~/.smart/main.rc' >> ~/.bashrc
