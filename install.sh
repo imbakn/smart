9c4ae2be1d8469600ae54a2d57ed5acf9f923a89
gaoxingyu <gaoxingyu@huaqin.com>
2018-04-14 18:32:03 +0800

    v0.22

--- install.sh
+++ install.sh
@@ -3,8 +3,15 @@
 # git clone https://github.com/imbakn/smartcmd ~/.smart
 # git clone https://github.com/imbakn/smart.personal ~/.smart/personal
 
+INSTALL_PATH=~/.smart
+
 [ -f ~/.bashrc ] && echo 'export SMART_SHELL=bash' >> ~/.bashrc
-[ -f ~/.bashrc ] && echo '[ -f ~/.smart/main.rc ] && source ~/.smart/main.rc' >> ~/.bashrc
+[ -f ~/.bashrc ] && echo "[ -f $INSTALL_PATH/main.rc ] && source $INSTALL_PATH/main.rc" >> ~/.bashrc
 
 [ -f ~/.zshrc ] && echo 'export SMART_SHELL=zsh' >> ~/.zshrc
-[ -f ~/.zshrc ] && echo '[ -f ~/.smart/main.rc ] && source ~/.smart/main.rc' >> ~/.zshrc
+[ -f ~/.zshrc ] && echo "[ -f $INSTALL_PATH/main.rc ] && source $INSTALL_PATH/main.rc" >> ~/.zshrc
+
+# need to backup orignal files
+# ln -s $INSTALL_PATH/configs/tmux/.tmux ~/.tmux
+# ln -s $INSTALL_PATH/configs/tmux/.tmux.conf ~/.tmux.conf
+# ln -s $INSTALL_PATH/personal/.tmuxinator ~/.tmuxinator
