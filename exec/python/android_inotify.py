5c6df86356073a8f5639ab34949731511c9d2cef
gaoxingyu <gaoxingyu@huaqin.com>
2018-04-15 23:07:02 +0800

    v0.45

--- exec/python/android_inotify.py
+++ exec/python/android_inotify.py
@@ -4,12 +4,26 @@ import pyinotify
 import os
 import sys
 
+try:
+    android_project_root = os.environ["T"]
+    android_product = os.environ["TARGET_PRODUCT"]
+    android_build_variant = os.environ["TARGET_BUILD_VARIANT"]
+except:
+    sys.exit(-1)
+
+if android_project_root == "":
+    sys.exit(0)
+
+if not os.path.isdir(android_project_root):
+    sys.exit(-2)
+
 if len(sys.argv) < 2:
     print("must input a dir to watch.")
     sys.exit(1)
 
 watch_event = pyinotify.IN_MODIFY | pyinotify.IN_DELETE | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
-watch_path = sys.argv[1]
+watch_path = android_project_root + "/" + sys.argv[1]
+
 
 class MyEventHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
@@ -26,19 +40,13 @@ class MyEventHandler(pyinotify.ProcessEvent):
 
 
 def handle_add(path):
-    print('file added', path[len(watch_path):len(path)])
-    remout = os.popen("adb remount")
-    ret = remout.read()
-    print(ret)
-
-    cmd = "adb push " + path + " /system/" + path[len(watch_path):len(path)]
-    print(cmd)
-    text = os.popen(cmd)
-    print(text.read())
+    with open(android_project_root + "/.system.change", "a+") as f:
+        f.write(path.replace("\n", "").replace("\r", "") + "\n")
 
 
 def handle_remove(path):
     print('file remove', path[len(watch_path):len(path)])
+
 
 wm = pyinotify.WatchManager()
 handler = MyEventHandler()
@@ -46,9 +54,8 @@ notifier = pyinotify.Notifier(wm, handler)
 
 wm.add_watch(watch_path, watch_event)
 for (root, dirs, files) in os.walk(watch_path):
-    for dirc in dirs:
-        tmppath = os.path.join(root, dirc)
-        # print(tmppath)
-        wm.add_watch(tmppath, watch_event)
+    for dir_ in dirs:
+        tmp_path = os.path.join(root, dir_)
+        wm.add_watch(tmp_path, watch_event)
 
 notifier.loop()
