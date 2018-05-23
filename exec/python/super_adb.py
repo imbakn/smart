92cbdd3095e56bc6ef7636b6811a03085238e234
gaoxingyu <gaoxingyu@huaqin.com>
2018-05-23 09:02:08 +0800

    donot reset shell

--- exec/python/super_adb.py
+++ exec/python/super_adb.py
@@ -76,9 +76,9 @@ def interact():
     shell = pexpect.spawn('adb -s %s shell' % call.device)
     shell.expect(['$','#'])
     shell.sendline('export PATH=/data/local/tmp/:$PATH')
-    shell.sendline('rm /data/local/tmp/reboot')
+    shell.sendline('[ -f  /data/local/tmp/reboot ] && rm /data/local/tmp/reboot')
     shell.sendline('alias ll="ls -l"')
-    shell.sendline('reset')
+    # shell.sendline('reset')
     shell.interact()
 
 def main():
