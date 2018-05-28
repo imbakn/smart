203cf0cb769d7e251b3a33aa8c83db32faba0286
gaoxingyu <gaoxingyu@huaqin.com>
2018-05-28 10:36:16 +0800

    add tag

---
+++ common.rc/tag.bash
@@ -0,0 +1,5 @@
+if hash ag 2>/dev/null; then
+  export TAG_SEARCH_PROG=ag  # replace with rg for ripgrep
+  tag() { command tag "$@"; source ${TAG_ALIAS_FILE:-/tmp/tag_aliases} 2>/dev/null; }
+  alias ag=tag  # replace with rg for ripgrep
+fi
