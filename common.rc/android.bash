febcfea9b59987f243a7576edc8ebe3c2bcc8b7c
imbakn <imbakn@gmail.com>
2018-05-31 21:47:59 +0800

    add cap and sap

--- common.rc/android.bash
+++ common.rc/android.bash
@@ -7,8 +7,6 @@ android_auto_jump()
        if [ -d "$T/$DIR" ]
        then
           cd $T/$DIR
-          cd ~
-          cd -
        else
           vim $T/$DIR
        fi
@@ -32,3 +30,64 @@ init_android_project()
     lunch $PRODUCT_VARIANT > /dev/null
     source $SMARTDIR/resource/init_android_env_var > /dev/null
 }
+
+usage()
+{
+    echo "you must run this command in a android project root dir."
+    echo "usage create_android_project PRODUCT_NAME PRODUCT_VARIANT"
+}
+
+
+create_android_project()
+{
+    local PRO_NAME=$1
+    local PRODUCT_VARIANT=$2
+    local PRO_PATH=$(pwd)
+
+    if [ ! -f ./build/envsetup.sh ]
+    then
+        usage
+    fi
+
+    if [ -f ~/.android.projects/$PRO_NAME ]
+    then
+        echo "already has a project called this name."
+        usage
+    fi
+
+    if [ -f ~/.android.projects ]
+    then
+        mv ~/.android.projects ~/.android.projects.bak
+    fi
+
+    if [ ! -d ~/.android.projects ]
+    then
+        mkdir -p ~/.android.projects
+    fi
+
+    echo "init_android_project $PRO_PATH $PRODUCT_VARIANT" >> ~/.android.projects/$PRO_NAME
+    alias $PRO_NAME="source ~/.android.projects/$PRO_NAME"   
+}
+
+start_android_project()
+{
+    local PRO_NAME=$1
+    source ~/.android.projects/$PRO_NAME
+}
+
+_start_android_project() {
+    COMPREPLY=()
+    local word
+    word="${COMP_WORDS[COMP_CWORD]}"
+
+    if [ "$COMP_CWORD" -eq 1 ]; then
+        local projects="$(compgen -W "$(ls ~/.android.projects)" -- "$word")"
+
+        COMPREPLY=( $commands $projects )
+    fi
+}
+complete -F _start_android_project start_android_project sap
+
+
+alias cap=create_android_project
+alias sap=start_android_project
