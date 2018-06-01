bc6cfdc395ebbdafc4312e8d1a47669379a16591
gaoxingyu <gaoxingyu@huaqin.com>
2018-06-01 10:54:32 +0800

    lg can jump to the line of vim

--- exec/bash/log_search.bash
+++ exec/bash/log_search.bash
@@ -7,7 +7,9 @@ fi
 
 if [ -f $1 ]
 then
-    cat $1 | nl -b a | peco
+    LINE=$(cat $1 | nl -b a | peco)
+    NUMBER=$(echo $LINE | awk '{print $1}')
+    vim $1 +$NUMBER
 else
     echo "$1 is not a file.."
 fi
