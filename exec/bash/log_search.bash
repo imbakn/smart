cd50c511abcd14a0d11e02adf800326652b5ae25
gaoxingyu <gaoxingyu@huaqin.com>
2018-06-06 15:39:37 +0800

    make log_search.bash better

--- exec/bash/log_search.bash
+++ exec/bash/log_search.bash
@@ -9,8 +9,17 @@ if [ -f $1 ]
 then
     while :
     do
-        LINE=$(cat $1 | nl -b a | peco)
+        if [ x"$QUERY" = "x" ];then
+            CONTENT=$(cat $1 | nl -b a | fzf --height 100 -e  --prompt=search: --border +s --print-query)
+        else
+            CONTENT=$(cat $1 | nl -b a | fzf --height 100 -e  --prompt=search: --border +s --print-query -q "$QUERY")
+        fi
+        LINE=$(echo "$CONTENT" | sed -n '2p')
+        QUERY=$(echo "$CONTENT" | sed -n '1p')
+        echo LINE $LINE
+        echo QUERY $QUERY
         NUMBER=$(echo $LINE | awk '{print $1}')
+        echo NUMBER $NUMBER
         if [ x"$NUMBER" != "x" ]; then
             vim $1 +$NUMBER
         else
