16726b5662872bfc1cbfab16dd02ef2600677f6f
gaoxingyu <gaoxingyu@huaqin.com>
2018-06-14 14:18:07 +0800

    add addUdevRules.bash

---
+++ exec/bash/addUdevRules.bash
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+SEARCHSTR="Qualcomm"
+if [ $# -gt 0 ];then
+    SEARCHSTR=$1
+fi
+
+PIDUID=$(lsusb | grep $SEARCHSTR | awk '{print $6}')
+VID=${PIDUID%:*}
+echo "vid is : "$VID
+PID=${PIDUID#*:}
+echo "pid is : "$PID
+echo "add this to /etc/udev/rules.d/70-android.rules"
+echo "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"$VID\", ATTRS{idProduct}==\"$PID\",MODE=\"0666\"" >> /etc/udev/rules.d/70-android.rules
+TMPLIST=$(cat /etc/udev/rules.d/70-android.rules)
+echo "$TMPLIST" | sort | uniq > /etc/udev/rules.d/70-android.rules
+
+
+
