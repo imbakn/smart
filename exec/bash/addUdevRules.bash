3f852a847747c25416a38c34b8462f12330d2274
gaoxingyu <gaoxingyu@huaqin.com>
2018-09-17 16:53:13 +0800

    fix addUdevRules.bash bug

--- exec/bash/addUdevRules.bash
+++ exec/bash/addUdevRules.bash
@@ -11,9 +11,10 @@ echo "vid is : "$VID
 PID=${PIDUID#*:}
 echo "pid is : "$PID
 echo "add this to /etc/udev/rules.d/70-android.rules"
-echo "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"$VID\", ATTRS{idProduct}==\"$PID\",MODE=\"0666\"" >> /etc/udev/rules.d/70-android.rules
+
+sudo bash -c "echo \"SUBSYSTEM==\\\"usb\\\", ATTRS{idVendor}==\\\"$VID\\\", ATTRS{idProduct}==\\\"$PID\\\",MODE=\\\"0666\\\"\" >> /etc/udev/rules.d/70-android.rules"
 TMPLIST=$(cat /etc/udev/rules.d/70-android.rules)
-echo "$TMPLIST" | sort | uniq > /etc/udev/rules.d/70-android.rules
+sudo bash -c "echo \"$TMPLIST\" | sort | uniq > /etc/udev/rules.d/70-android.rules"
 
 
 
