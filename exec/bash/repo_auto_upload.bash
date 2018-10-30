c2dbdc7cc8ef83a4d5c14a71d19938c04a887224
gaoxingyu <gaoxingyu@huaqin.com>
2018-10-30 21:44:15 +0800

    add rpb and rpf

--- exec/bash/repo_auto_upload.bash
+++ exec/bash/repo_auto_upload.bash
@@ -1,7 +1,19 @@
 #! /bin/bash
 
 git add "$@"
+echo ============$UPLOAD_TYPE
+projectName=$(git config --get remote.gerrit.projectname)
+rawName=${projectName##*/}
+PNAME=$(cat $T/.project.name)
+FIX_PROJECT_NAME="["$rawName"]"
+FIX_TYPE_BUG="[BUG]"
+FIX_TYPE_FEATURE="[FEATURE]"
+FIX_TYPE=$FIX_TYPE_FEATURE
+if [ x"$UPLOAD_TYPE" = "xbug" ]
+then
+    FIX_TYPE=$FIX_TYPE_BUG
+fi
+FIX_PNAME="["$PNAME"]"
+WHOLE_NAME=$FIX_PNAME$FIX_TYPE$FIX_PROJECT_NAME
+git commit -e -m "$WHOLE_NAME" && repoupload
 
-git commit
-
-repoupload
